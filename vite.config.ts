14f3265b4bdda2ad59582ba7df572999eca50cab
Gingerbreadfork <gingerbreadfork@protonmail.com>
2022-05-01 22:28:09 +1000

    Add dist to .gitignore & Fixed Vite Config for Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,7 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte({
-      onwarn: (warning, handler) => {
-        const { code, frame } = warning
-        if (code === 'css-unused-selector') return
-
-        handler(warning)
-      },
-    }),
-    viteSingleFile(),
-  ],
-  build: {
-    target: 'esnext',
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-    cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => 'everything.js',
-      },
-    },
-  },
-})
+    plugins: [svelte()],
+});
